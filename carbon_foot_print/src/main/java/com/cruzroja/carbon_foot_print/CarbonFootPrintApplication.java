293a2bf6f5eedd1f087987d90c079288b4c7c907
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-04-04 20:33:24 -0500

    Creacion de arquitectura hexagonal

---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/CarbonFootPrintApplication.java
@@ -0,0 +1,13 @@
+package com.cruzroja.carbon_foot_print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonFootPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonFootPrintApplication.class, args);
+	}
+
+}
---
+++ carbon_foot_print/src/test/java/com/cruzroja/carbon_foot_print/CarbonFootPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruzroja.carbon_foot_print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonFootPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
