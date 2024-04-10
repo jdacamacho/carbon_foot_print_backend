f78be171e1ca0289665b2b25fb6a7b847f463020
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-04-10 18:13:49 -0500

    actualizacion de lsita de permisos en el import.sql

--- carbon_foot_print/src/main/resources/import.sql
+++ carbon_foot_print/src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO permissions(idPermission, name, description) VALUES (101, "ROLE_Crea
 INSERT INTO permissions(idPermission, name, description) VALUES (102, "ROLE_Actualizar_Roles","Permiso para actualizar los roles en el sistema");
 INSERT INTO permissions(idPermission, name, description) VALUES (103, "ROLE_Eliminar_Roles","Permiso para eliminar los roles en el sistema");
 INSERT INTO permissions(idPermission, name, description) VALUES (104, "ROLE_Consultar_Rol","Permiso para consultar un rol en el sistema");
+INSERT INTO permissions(idPermission, name, description) VALUES (105, "ROLE_Listar_permisos","Permiso para listar permisos");
