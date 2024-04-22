0382b149eb466aa3c52658291b738939df733a42
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-04-22 11:58:26 -0500

    JwtService

---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Configuration/SecurityConfigurations.java
@@ -0,0 +1,5 @@
+package com.cruzroja.carbon_foot_print.Infrastucture.Configuration;
+
+public class SecurityConfigurations {
+    
+}
---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/JWT/JwtService.java
@@ -0,0 +1,76 @@
+package com.cruzroja.carbon_foot_print.Infrastucture.JWT;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    
+    private String getToken(Map<String,Object> extraClaim, UserDetails user) {
+        return Jwts
+            .builder()
+            .setClaims(extraClaim)
+            .setSubject(user.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+            .signWith(getKey(),SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+
+    public String getToken(UserDetails user){
+        return getToken(new HashMap<>(),user);
+    }
+
+    private Claims getAllClaims(String token){
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    public <T> T getClaim(String token, Function <Claims,T> claimResolver){
+        final Claims claims = getAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token){
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+}
+
+
