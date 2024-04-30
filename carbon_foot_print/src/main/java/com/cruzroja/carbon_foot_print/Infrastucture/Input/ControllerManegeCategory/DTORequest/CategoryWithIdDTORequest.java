4eb7c603b728121ddbbba02948072b871faa44a8
sBenitezL <sbenitez1607@gmail.com>
2024-04-29 23:08:41 -0500

    Fix: Loombok anotations in DTORequest Add: MapperCategoryInfraestructureDomain

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/DTORequest/CategoryWithIdDTORequest.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/DTORequest/CategoryWithIdDTORequest.java
@@ -4,7 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class CategoryWithIdDTORequest {
     @Positive(message = "categoryId must be positive")
     private long categoryId;
