1d61d21ff087fc84b9cc8a295a7f136606d0cae4
sBenitezL <sbenitez1607@gmail.com>
2024-04-29 23:08:41 -0500

    Fix: Loombok anotations in DTORequest Add: MapperCategoryInfraestructureDomain

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/DTOResponse/CategoryDTOResponse.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/DTOResponse/CategoryDTOResponse.java
@@ -1,5 +1,13 @@
 package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCategory.DTOResponse;
 
-public class CategoryDTOResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class CategoryDTOResponse {
+    private long categoryId;
+    private String categoryName;
+    private String categoryDescription;
+    private String categoryScope;
 }
--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/mappers/MapperCategoryInfraestructureDomain.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCategory/mappers/MapperCategoryInfraestructureDomain.java
@@ -1,14 +1,21 @@
 package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCategory.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 import com.cruzroja.carbon_foot_print.Domain.Models.Category;
 import com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCategory.DTORequest.CategoryDTORequest;
 import com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCategory.DTORequest.CategoryWithIdDTORequest;
+import com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCategory.DTOResponse.CategoryDTOResponse;
 
 @Mapper(componentModel = "spring")
 public interface MapperCategoryInfraestructureDomain {
     Category mapRequestModel(CategoryDTORequest request);
 
     Category mapRequestModel(CategoryWithIdDTORequest request);
+
+    CategoryDTOResponse mapModelToResponse(Category model);
+
+    List<CategoryDTOResponse> mapModelsToResponse(List<Category> cotegories);
 }
