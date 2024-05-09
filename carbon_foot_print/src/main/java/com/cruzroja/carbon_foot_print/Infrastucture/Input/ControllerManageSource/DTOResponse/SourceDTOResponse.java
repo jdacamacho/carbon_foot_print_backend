a4502eaf752d045b06e60ac32e859381e64d660f
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-05-09 12:46:39 -0500

    controladores de source, a espera de probarlo

---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManageSource/DTOResponse/SourceDTOResponse.java
@@ -0,0 +1,15 @@
+package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManageSource.DTOResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SourceDTOResponse {
+    private long idSource;
+    private String sourceName;
+
+    public SourceDTOResponse(){
+        
+    }
+}
