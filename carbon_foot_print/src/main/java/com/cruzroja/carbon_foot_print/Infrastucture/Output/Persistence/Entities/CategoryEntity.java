e3c642e92871d3fc23fe2a379c9c7c8ec6bfb7d8
sBenitezL <sbenitez1607@gmail.com>
2024-05-09 11:36:39 -0500

    Update: Category entity

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/CategoryEntity.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/CategoryEntity.java
@@ -1,10 +1,16 @@
 package com.cruzroja.carbon_foot_print.Infrastucture.Output.Persistence.Entities;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +23,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class CategoryEntity {
-    
+
     /** Identificador único de la categoría. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,6 +40,13 @@ public class CategoryEntity {
     /** Ámbito de la categoría. */
     @Column(nullable = false)
     private String categoryScope;
+
+    @Column(nullable = false)
+    private boolean categoryState;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "category_pollution", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "pollution_type_id"))
+    private List<PollutionTypeEntity> pollutions;
 
     /**
      * Constructor de la clase CategoryEntity.
