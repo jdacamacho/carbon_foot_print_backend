bd58b271f579edce054145e57c9defa3e977b609
sBenitezL <sbenitez1607@gmail.com>
2024-05-20 12:56:20 -0500

    Se agregan las notaciones Date y AllArgsConstructor

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCalculator/DTOResponse/CalculatorResponseDTOResponse.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeCalculator/DTOResponse/CalculatorResponseDTOResponse.java
@@ -2,6 +2,11 @@ package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeCalcu
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CalculatorResponseDTOResponse {
     private double total;
     private List<SourceResultDTOResponse> totalBySources;
