0d83ca96d27dd65cfba88d29e95228b2a6b8ed34
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-05-22 19:50:23 -0500

    gateway plan compensaion

---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Application/Output/ManageCompensationPlanGatewayIntPort.java
@@ -0,0 +1,14 @@
+package com.cruzroja.carbon_foot_print.Application.Output;
+
+import java.util.List;
+
+import com.cruzroja.carbon_foot_print.Domain.Models.CompensationPlan;
+
+public interface ManageCompensationPlanGatewayIntPort {
+    public List<CompensationPlan> findAll();
+    public CompensationPlan save(CompensationPlan compensationPlan);
+    public CompensationPlan findById(long planId);
+    public CompensationPlan findByName(String planName);
+    public boolean existsById(long planId);
+    public boolean existsByName(String planName);
+}
--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Repositories/CompensationPlanRepository.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Repositories/CompensationPlanRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.cruzroja.carbon_foot_print.Infrastucture.Output.Persistence.Entities.CompensationPlanEntity;
 
 public interface CompensationPlanRepository extends CrudRepository<CompensationPlanEntity,Long> {
-    
+    public CompensationPlanEntity findByPlanName(String planName);
+    public boolean existsByPlanName(String planName);
 }
