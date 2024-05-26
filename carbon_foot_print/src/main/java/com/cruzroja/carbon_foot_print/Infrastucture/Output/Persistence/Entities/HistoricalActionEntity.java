6dac4cbf139345a8b29a2f7134a5c89621eff0cf
sBenitezL <sbenitez1607@gmail.com>
2024-05-26 16:25:06 -0500

    Add: Historical Action entity

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/HistoricalActionEntity.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/HistoricalActionEntity.java
@@ -1,10 +1,14 @@
 package com.cruzroja.carbon_foot_print.Infrastucture.Output.Persistence.Entities;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,14 +22,22 @@ public class HistoricalActionEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long historicalActionId;
 
-    @Column(nullable = false, length = 45)
+    @Column(nullable = false, length = 45, updatable = false)
     private String historicalActionName;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, updatable = false)
     private String historicalActionDescription;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private double historicalActionUnitaryPrice;
+    @Column(nullable = false, updatable = false)
+    private Date histaricalActionEffectiveDate;
+    @Column(nullable = true)
+    private Date histaricalActionCloseDate;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "actionId")
+    ActionEntity action;
 
     public HistoricalActionEntity() {
     }
