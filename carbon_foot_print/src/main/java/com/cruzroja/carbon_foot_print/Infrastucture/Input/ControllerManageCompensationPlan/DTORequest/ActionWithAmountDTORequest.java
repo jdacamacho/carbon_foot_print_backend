7d9ad1d1a094d0d244afd666893c176052fd7b34
sBenitezL <sbenitez1607@gmail.com>
2024-05-27 16:45:04 -0500

    Add: Action with amount

---
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManageCompensationPlan/DTORequest/ActionWithAmountDTORequest.java
@@ -0,0 +1,20 @@
+package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManageCompensationPlan.DTORequest;
+
+import com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManageAction.DTORequest.ActionWithIdDTORequest;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ActionWithAmountDTORequest {
+    @NotNull(message = "action can't be null.")
+    private ActionWithIdDTORequest action;
+    @Positive(message = "compensationActionAmount must be greater than 0.")
+    private int compensationActionAmount;
+
+    public ActionWithAmountDTORequest() {
+    }
+}
