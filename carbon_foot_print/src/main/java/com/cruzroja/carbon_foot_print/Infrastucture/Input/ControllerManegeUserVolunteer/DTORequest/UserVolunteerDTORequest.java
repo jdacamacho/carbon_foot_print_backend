8618de7c2b2f9e5d41ce8323c5aa29d019ce254e
Simon Guzman <siguzman@unicauca.edu.co>
2024-06-03 15:05:10 -0500

    Documentacion modulo de voluntarios capa de infraestructura

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeUserVolunteer/DTORequest/UserVolunteerDTORequest.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Input/ControllerManegeUserVolunteer/DTORequest/UserVolunteerDTORequest.java
@@ -1,3 +1,8 @@
+/**
+ * @file UserVolunteerDTORequest.java
+ * @brief Clase DTO para la creación de un voluntario.
+ */
+
 package com.cruzroja.carbon_foot_print.Infrastucture.Input.ControllerManegeUserVolunteer.DTORequest;
 
 import com.cruzroja.carbon_foot_print.Infrastucture.Input.UserDTO.DTORequest.UserDTORequest;
@@ -9,15 +14,39 @@ import lombok.Setter;
 
 import java.util.List;
 
+/**
+ * @class UserVolunteerDTORequest
+ * @brief DTO para la creación de un voluntario.
+ * 
+ * Esta clase implementa un DTO para la creación de un voluntario.
+ */
 @Getter
 @Setter
 public class UserVolunteerDTORequest extends UserDTORequest {
+    /** @brief Posición del voluntario. */
     @NotBlank(message = "position can't be empty")
     private String position;
 
+    /**
+     * @brief Constructor de la clase.
+     */
     public UserVolunteerDTORequest() {
     }
 
+    /**
+     * @brief Constructor de la clase.
+     * 
+     * @param documentNumber Número de documento.
+     * @param documentType Tipo de documento.
+     * @param names Nombres.
+     * @param lastNames Apellidos.
+     * @param personalPhone Teléfono personal.
+     * @param personalEmail Correo electrónico personal.
+     * @param username Nombre de usuario.
+     * @param password Contraseña.
+     * @param roles Lista de roles.
+     * @param position Posición.
+     */    
     public UserVolunteerDTORequest(long documentNumber, String documentType,
             String names, String lastNames, String personalPhone,
             String personalEmail, String username, String password,
