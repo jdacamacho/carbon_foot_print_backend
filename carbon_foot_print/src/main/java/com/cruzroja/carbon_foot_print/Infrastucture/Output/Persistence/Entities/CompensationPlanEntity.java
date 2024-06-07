5a1210eb1db381730ce3a5eebe2879c70a03a081
sBenitezL <sbenitez1607@gmail.com>
2024-06-07 12:20:32 -0500

    Fix: relationship between volunteer and compensation plan

--- carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/CompensationPlanEntity.java
+++ carbon_foot_print/src/main/java/com/cruzroja/carbon_foot_print/Infrastucture/Output/Persistence/Entities/CompensationPlanEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -45,7 +46,7 @@ public class CompensationPlanEntity {
     private boolean planDefault;
 
     @JoinColumn(nullable = false, name = "valunteerId")
-    @OneToOne(optional = false)
+    @ManyToOne(optional = false)
     private UserVolunteerEntity volunteer;
 
     public CompensationPlanEntity() {
